b6723fe3a04a5365f6a9a8c9536883584de602ae
amassiro <massironi.andrea@gmail.com>
2013-08-15 15:37:46 +0200

    update nsel 5, 6, 7

---
+++ scripts/doFig10.sh
@@ -0,0 +1,6 @@
+# 
+# 
+root -l -q finalPlot.C+\(6,1,\"M_{ll}\ \(GeV\)\",\"\",\"mll-VBF-2011.root\",\"mll_vbf\",0,125,4.9,1,1\)
+root -l -q finalPlot.C+\(6,1,\"M_{ll}\ \(GeV\)\",\"\",\"mll-VBF-2012.root\",\"mll_vbf\",0,125,19.5,1,1\)
+# 
+# 
