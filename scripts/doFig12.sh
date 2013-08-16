87311972dc0834bd3fd2b0891303c00a645a4203
amassiro <massironi.andrea@gmail.com>
2013-08-16 10:40:20 +0200

    update nsel 5, 6, 7

--- scripts/doFig12.sh
+++ scripts/doFig12.sh
@@ -1,5 +1,5 @@
 # 
 # 
-root -l -q finalPlot.C+\(7,1,\"M_{ll}\ \(GeV\)\",\"\",\"mll-VH-2012.root\",\"mll_vh\",0,125,19.5,1,1\)
+root -l -q finalPlot.C+\(7,1,\"M_{ll}\",\"GeV\",\"mll-VH-2012.root\",\"mll_vh\",0,125,19.5,1,1\)
 # 
 # 
