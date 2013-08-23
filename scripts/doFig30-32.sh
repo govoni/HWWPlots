1c1ff91191d344820cfa2f7bbb2512887cd6ea9e
jaehyeok <jaehyeokyoo@gmail.com>
2013-08-22 17:51:26 -0700

    scripts to produce jae's plots

---
+++ scripts/doFig30-32.sh
@@ -0,0 +1,11 @@
+# Top control regions plots 
+# 
+# 
+root -b -q finalPlot.C+'(9,1,"M_{T}",   "GeV","plotrootfile_topcontrol_mT_of_0j.root",    "topcontrol_mT_of_0j",   0,125,19.5,1,1)';
+root -b -q finalPlot.C+'(9,1,"M_{ll}",  "GeV","plotrootfile_topcontrol_mll_of_0j.root",   "topcontrol_mll_of_0j",  0,125,19.5,1,1)';
+root -b -q finalPlot.C+'(9,1,"M_{T}",   "GeV","plotrootfile_topcontrol_mT_of_1j.root",    "topcontrol_mT_of_1j",   0,125,19.5,1,1)';
+root -b -q finalPlot.C+'(9,1,"M_{ll}",  "GeV","plotrootfile_topcontrol_mll_of_1j.root",   "topcontrol_mll_of_1j",  0,125,19.5,1,1)';
+root -b -q finalPlot.C+'(9,1,"M_{T}",   "GeV","plotrootfile_topcontrol_mT_incl_2j.root",  "topcontrol_mT_incl_2j", 0,125,19.5,1,1)';
+root -b -q finalPlot.C+'(9,1,"M_{ll}",  "GeV","plotrootfile_topcontrol_mll_incl_2j.root", "topcontrol_mll_incl_2j",0,125,19.5,1,1)';
+# 
+# 
