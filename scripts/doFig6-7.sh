ddf97dc028498605960316edcd6fbe51966bfeac
Guillelmo Gomez Ceballos Retuerto <ceballos@dtmit02.cern.ch>
2013-09-02 11:51:10 +0200

    new

--- scripts/doFig6-7.sh
+++ scripts/doFig6-7.sh
@@ -1,7 +1,7 @@
 # 
 # stacked plots for data  - bkgs plots  
 # 
-root -b -q finalPlot.C+'(9, 1, "M_{ll}", "GeV", "inputrootfiles/plotrootfile_stackdataminusbkg_mll.root", "st_mll", 0, 125, 24.4, 1, 1)';
-root -b -q finalPlot.C+'(9, 1, "M_{T}",  "GeV", "inputrootfiles/plotrootfile_stackdataminusbkg_mT.root",  "st_mT",  0, 125, 24.4, 1, 1)';
+root -q -b HWWPlots/finalPlot.C+'(10,1,"m_{T}",  "GeV", "rootFiles/plotrootfile_stackdataminusbkg_mT.root",  "st_mT",  0, 125, 24.4, 0, 1)';		# fig 6 left
+root -q -b HWWPlots/finalPlot.C+'(10,1,"m_{\\ell\\ell}", "GeV", "rootFiles/plotrootfile_stackdataminusbkg_mll.root", "st_mll", 0, 125, 24.4, 0, 1)';		# fig 7 left
 # 
 # 
